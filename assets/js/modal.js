afc31d492047d165044b58843da3b8eade96445d
Aakash Pereira <25438913+TheDarthHazard@users.noreply.github.com>
2024-01-07 21:14:44 +0000

    Permanent modal dismissal implemented
    Welcome modal when dismissed will remain dismissed on future visits. Currently implemented using local storage

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -4,7 +4,16 @@ const pageContent = document.getElementById("page-content")
 
 const welcomeModal = document.getElementById("welcome-modal")
 
+const welcomeModalSeen = localStorage.getItem("welcomeModalSeen");
+
+if(!welcomeModalSeen) {
+    welcomeModal.style.display = "block";
+} else {
+    viewIndex()
+}
+
 closeBtn.addEventListener("click", removeModal)
+
 closeBtn.addEventListener("keypress", function(e) {
     if(e.key == "Enter") {
         removeModal()
@@ -12,8 +21,13 @@ closeBtn.addEventListener("keypress", function(e) {
 })
 
 function removeModal() {
+    localStorage.setItem("welcomeModalSeen", "true")
     welcomeModal.style.display = "none"
+    viewIndex()
+    pageContent.focus()
+}
+
+function viewIndex() {
     pageContent.style.opacity = "1"
     pageContent.removeAttribute("inert")
-    pageContent.focus()
 }
