7511871444bcbb9dd13aa00cacb013369bd89d7c
Aakash Pereira <25438913+TheDarthHazard@users.noreply.github.com>
2025-04-30 20:59:22 +0100

    Accessibility fixes
    - iOS focus not managed when dismissing alert
    - Alert close icon button colour set to black to fix contrast issues
    - "|" decorative character hidden from assistive technology

--- src/js/alert.js
+++ src/js/alert.js
@@ -1,6 +1,8 @@
 const closeAlertBtn = document.getElementById("alert-close-btn")
 const alertEl = document.getElementById("alert-el")
+const logoEl = document.getElementById("logo-el")
 
 closeAlertBtn.addEventListener("click", function() {
     alertEl.style.display = "none";
+    logoEl.focus();
 })
