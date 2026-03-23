c4e52660925bc2fe844a0a108dc00242e26eb4cb
Aakash Pereira <25438913+TheDarthHazard@users.noreply.github.com>
2026-03-23 21:44:52 +0000

    Homepage changes
    - Hero section with video added including HTML and CSS additions
    - Accessibility improvements including skip to main content link and aria-expanded for hamburger menu button

--- src/js/main.js
+++ src/js/main.js
@@ -6,5 +6,7 @@ const hamburgerBtn = document.getElementById("hamburger-btn")
 const mobileMenu = document.getElementById("mobile-menu")
 
 hamburgerBtn.addEventListener("click", function() {
+    let hamburgerExpandedState = hamburgerBtn.getAttribute("aria-expanded") === "true"
     mobileMenu.classList.toggle("hidden")
+    hamburgerBtn.setAttribute("aria-expanded", `${!hamburgerExpandedState}`)
 })
